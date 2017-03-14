26bd6f9077f631d81da62da73f90fdc8e04fb9ff
Nick Fallon <nickfallonwastaken@gmail.com>
2017-03-14 15:30:54 -0600

    X button working

--- public/js/select.js
+++ public/js/select.js
@@ -9,17 +9,27 @@ var food = window.location.search.split("=")[1]
 
 function getRecipes() {
   var url = "https://api.edamam.com/search?q="
-    url += (food) + "&app_id=da1c76bb&app_key=ea9691b0b8fa631d00f86f74276ce140&from=0&to=29"
+    url += (food) + "&app_id=da1c76bb&app_key=ea9691b0b8fa631d00f86f74276ce140&from=0&to=99"
     $.get(url)
       .then(function(data) {
       var image = ""
       var title = ""
-        for (var i = 0; i < 29; i++) {
+      var ingredients = []
+      var ingredientsList = []
+        for (var i = 0; i < 30; i++) {
           image = data.hits[i].recipe.image
           title = data.hits[i].recipe.label
-          $("#main").append("<img src=" + image + ">")
-          $("#main").append("<h3>" + title + "</h3>")
+          ingredientsList = data.hits[i].recipe.ingredientLines
+          $("#main").append("<div class='get" + i + "'><img src=" + image + "><h3>" + title + "</h3></div>")
         }
+
     })
 }
 getRecipes()
+
+var index = 0
+
+$("#exmark").click(function (){
+  $(".get" + index).remove()
+  index++
+})
