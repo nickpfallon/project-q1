56ee72de7b5c23ecbb479290e181bc17622bf4cd
Nick Fallon <nickfallonwastaken@gmail.com>
2017-03-13 19:45:25 -0600

    Added some styling to index

--- public/js/select.js
+++ public/js/select.js
@@ -9,13 +9,17 @@ var food = window.location.search.split("=")[1]
 
 function getRecipes() {
   var url = "https://api.edamam.com/search?q="
-    url += (food) + "&app_id=da1c76bb&app_key=ea9691b0b8fa631d00f86f74276ce140&from=0&to=3"
+    url += (food) + "&app_id=da1c76bb&app_key=ea9691b0b8fa631d00f86f74276ce140&from=0&to=29"
     $.get(url)
       .then(function(data) {
-      var image = data.hits[0].recipe.image
-      var title = data.hits[0].recipe.label
-      $("#main").append("<img src=" + image + ">")
-      $("#main").append("<h3>" + title + "</h3>")
+      var image = ""
+      var title = ""
+        for (var i = 0; i < 29; i++) {
+          image = data.hits[i].recipe.image
+          title = data.hits[i].recipe.label
+          $("#main").append("<img src=" + image + ">")
+          $("#main").append("<h3>" + title + "</h3>")
+        }
     })
 }
 getRecipes()
